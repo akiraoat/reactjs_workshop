9a57b34872a06d5c6df575c221da2fe0f7af12b9
akiraoat <45424207+akiraoat@users.noreply.github.com>
2020-02-15 16:23:02 +0700

    inital

--- workshop1/src/App.js
+++ workshop1/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
-import Signin from "./views/authentication/Signin";
-import Signup from "./views/authentication/Signup";
-import Fulllayout from "./layouts/fulllaouts";
+//import Signin from "./views/authentication/Signin";
+//import Signup from "./views/authentication/Signup";
+//import Fulllayout from "./layouts/fulllaouts";
+
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Blanklayout from "./layouts/blanklayout";
 
 class App extends React.Component {
   render() {
-    return <Fulllayout />;
+    return (
+      <HashRouter>
+        <Switch>
+          <Route path="/" name="authenticate" Component={Blanklayout} />
+        </Switch>
+      </HashRouter>
+    );
   }
 }
+
 export default App;
---
+++ workshop1/src/layouts/blanklayout.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import authRoutes from "../routes/authroutes";
+
+class Blanklayout extends React.Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          {authRoutes.map((route, idx) => {
+            return route.component ? (
+              <Route
+                key={idx}
+                path={route.path}
+                exact={route.exact}
+                name={route.name}
+                render={props => <route.component {...props} />}
+              />
+            ) : null;
+          })}
+          <Redirect from="/" to="/authentication/signin" />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Blanklayout;
