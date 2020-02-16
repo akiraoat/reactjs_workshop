101fccd164aec5058e260fb2459ef1e8d241fe8f
akiraoat <45424207+akiraoat@users.noreply.github.com>
2020-02-16 16:55:14 +0700

    inital

---
+++ API/application/models/AuthenModel.php
@@ -0,0 +1,72 @@
+<?php
+class AuthenModel extends CI_model {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    //ใช้สำหรับดึงข้อมูลจากฐานข้อมูลมาแสดง
+    public function getUsers()
+     {
+        $sql = " select * from users ";
+        $result =  $this->db->query($sql);
+        $arrResult = $result->result();
+        return $arrResult;
+     }
+
+    //ใช้สำหรับอัพเดทข้อมูล เพื่อยกเลิกข้อมูลผู้ใช้งาน
+    public function cancelData($username)
+    {
+        $result = "";
+        try {
+            $this->db->trans_start();
+            $this->db->where('username', $username);
+            $this->db->update('users', array('status' => '0'));
+            $this->db->trans_commit();
+            $result = 1;
+        } catch (Exception $e) {
+            $this->db->trans_rollback();
+            $result = 0;
+        }
+        return $result;
+	}
+
+    public function register($username, $password, $email, $name)
+    {
+        $sql = "SELECT *
+        FROM users
+        WHERE username = '" . $username . "'";
+
+        $query = $this->db->query($sql);
+        $result = $query->result();
+        $rows = count($query->result());
+        if ($rows > 0) {
+           $rs = array([
+                'username' => $username,
+                'status' => "0",
+                'message' => "ชื่อผู้ใช้งานนี้ เคยลงทะเบียนแล้ว",
+            ]);
+           
+        } else {
+            $insert_data = array(
+                'username' => $username,
+                'password' => $password,
+                'email' => $email,
+                'status' => '1',
+                'name' => $name,
+ 
+            );
+            $this->db->insert('users', $insert_data);
+        
+            $rs = array([
+                'username' => $username,
+                'status' => "1",
+                'message' => "ลงทะเบียนสำเร็จ",
+            ]);
+        }
+
+        return $rs;
+    }
+}
+?>
